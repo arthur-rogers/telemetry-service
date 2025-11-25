ff115798f4ff3f931148e2843dc4e35ab483264b
Arthur Rogers <a23ryndin@gmail.com>
2025-11-25 23:06:13 +0300

    build: small script to avoid issues with imports in jsdoc

---
+++ jsdoc-ignore-imports.js
@@ -0,0 +1,8 @@
+export const handlers = {
+  newDoclet: function (e) {
+    if (e.doclet.ignored) return;
+    if (e.doclet.comment && e.doclet.comment.includes('@import')) {
+      e.doclet.ignore = true;
+    }
+  },
+};
